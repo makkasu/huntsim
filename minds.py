adc2539984769dc09c617b13cbc3bff0fee701c0
oligiles0 <oligiles0@gmail.com>
2017-06-22 17:23:47 +0100

    Added base keras model

--- minds.py
+++ minds.py
@@ -6,13 +6,14 @@ Description:
 	- Contains creatures' neural networks
 """
 
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Activation
 import random
 import numpy as np
 
 class Mind():
-	def __init__(self, numTiles, neuronsPerLayer, numLayers, firstGeneration, DNA):
-		self.inputNeurons = numTiles
+	def __init__(self, numTiles=25, neuronsPerLayer=50, numLayers=3, firstGeneration, DNA):
+		self.inputCount = numTiles
 		self.outputNeurons = 5 #up, down, left, right, speed
 
 		if firstGeneration:
@@ -23,13 +24,22 @@ class Mind():
 			self.l2Biases = 
 			self.outWeights =
 			self.outBiases =
-
+			self.DNA = '' #All floats of all tensors converted into a string of bits
 		else:
-			#Convert DNA to numpy.ndarrays
+			#Convert DNA to weights/biases
 
-		self.DNA = '' #All floats of all tensors converted into a string of bits
+		#Define Keras model of the brain, using sequential layers of activation neurons
+		#Set weights using DNA : https://keras.io/models/about-keras-models/
+		model = Sequential()
+		model.add(Dense(neuronsPerLayer, input_shape = (inputCount,)))
+		model.add(Activation('relu'))
+		for i in range(numLayers - 1):
+			model.add(Dense(neuronsPerLayer))
+			model.add(Activation('relu'))
+		model.add(Dense(outputNeurons))
+		model.add(Activation('relu'))
 
-def think(tiles, DNA):
-	#Use the creatures DNA to construct tensors of weights/biases and feed tiles through
-	pass
-
+def think(mind, vision):
+	#Use the creatures Keras model and vision to determine direction/speed
+	actions = mind.predict(vision)
+	return actions
