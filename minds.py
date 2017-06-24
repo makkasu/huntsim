a06ecc7f53a9add97020a4684e6b261e58e9151e
oligiles0 <oligiles0@gmail.com>
2017-06-24 22:21:21 +0100

    Normalised weights

--- minds.py
+++ minds.py
@@ -24,8 +24,9 @@ class Mind():
 
 		if self.firstGeneration:
 			#Generate random DNA for the first generation
-			self.DNAlength = (self.inputCount * self.neuronsPerLayer + ((self.neuronsPerLayer * 
-				self.neuronsPerLayer) * self.numLayers) + self.neuronsPerLayer * self.outputNeurons)
+			self.DNAlength = (self.inputCount * self.neuronsPerLayer + self.neuronsPerLayer +
+				((self.neuronsPerLayer * self.neuronsPerLayer) * self.numLayers) + self.neuronsPerLayer * 
+				self.outputNeurons + self.neuronsPerLayer * self.numLayers + self.outputNeurons)
 			self.DNAbin = []
 			for i in range(0, self.DNAlength * 32):
 				self.DNAbin.append(str(random.randint(0,1)))
@@ -35,39 +36,81 @@ class Mind():
 			self.DNA = []
 			for i in range(0, len(self.DNAbin), 32):
 				self.b.bin = self.DNAbin[i:i+32]
-				self.DNA.append([0.0 if math.isnan(self.b.float) else self.b.float])
+				self.DNA.append([0.000001 if math.isnan(self.b.float) else self.b.float])
 
 		#Convert DNA to weights
 		self.weights = []
 		self.count = 0
+		self.DNA = (self.DNA-np.amin(self.DNA))/(np.amax(self.DNA)-np.amin(self.DNA)) - 0.5 #TEST NORMALISATION - SEEMS TO WORK
+
+		#Input weights: inputCount x neuronsPerLayer matrix
 		self.l = np.array(self.DNA[self.count:self.count + self.inputCount*self.neuronsPerLayer])
 		self.count += self.inputCount*self.neuronsPerLayer
 		self.l = self.l.reshape(self.inputCount, self.neuronsPerLayer)
 		self.weights.append(self.l)
+		#Input activation weights: neuronsPerLayer array
+		self.l = np.array(self.DNA[self.count:self.count + self.neuronsPerLayer])
+		self.count += self.neuronsPerLayer
+		self.l = self.l.reshape(self.neuronsPerLayer)
+		self.weights.append(self.l)
+
 		for i in range(self.numLayers):
+			#Hidden layer weights: neuronsPerLayer x neuronsPerLayer matrix
 			self.l = np.array(self.DNA[self.count:self.count + self.neuronsPerLayer*self.neuronsPerLayer])
 			self.count += self.neuronsPerLayer*self.neuronsPerLayer
 			self.l = self.l.reshape(self.neuronsPerLayer, self.neuronsPerLayer)
 			self.weights.append(self.l)
+			#Hidden activation weights: neuronsPerLayer array
+			self.l = np.array(self.DNA[self.count:self.count + self.neuronsPerLayer])
+			self.count += self.neuronsPerLayer
+			self.l = self.l.reshape(self.neuronsPerLayer)
+			self.weights.append(self.l)
+		
+		#Output layer weights: neuronsPerLayer x outputNeruons matrix
 		self.l = np.array(self.DNA[self.count:self.count + self.outputNeurons*self.neuronsPerLayer])
 		self.l = self.l.reshape(self.neuronsPerLayer, self.outputNeurons)
 		self.weights.append(self.l)
 		self.count += self.neuronsPerLayer * self.outputNeurons
+		#Hidden activation weights: neuronsPerLayer array
+		self.l = np.array(self.DNA[self.count:self.count + self.neuronsPerLayer])
+		self.count += self.neuronsPerLayer
+		self.l = self.l.reshape(self.outputNeurons)
+		self.weights.append(self.l)
+
 
 		#Define Keras model of the brain, using sequential layers of activation neurons
 		self.model = Sequential()
-		self.model.add(Dense(self.neuronsPerLayer, input_shape = (self.inputCount,), activation='relu'))
+		self.model.add(Dense(self.neuronsPerLayer, input_shape = (self.inputCount,), activation='sigmoid'))
 		for i in range(self.numLayers):
-			self.model.add(Dense(self.neuronsPerLayer, activation='relu'))
-		self.model.add(Dense(self.outputNeurons, activation='relu'))
+			self.model.add(Dense(self.neuronsPerLayer, activation='sigmoid'))
+		self.model.add(Dense(self.outputNeurons, activation='sigmoid'))
 		#print self.weights
 		for arr in self.model.get_weights():
 			#print arr
-			print arr.shape
+			#print arr.shape
+			pass
 		for arr in self.weights:
-			#print arr
-			print arr.shape
-		#self.model.set_weights(self.weights)
+			print arr
+			#print arr.shape
+			pass
+		# weights = []
+		# # # weights.append(np.random.rand(25, 20))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (25, 20)))
+		# #weights.append(np.zeros(20,))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (20,)))
+		# # # weights.append(np.random.rand(20, 20))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (20, 20)))
+		# #weights.append(np.zeros(20,))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (20,)))
+		# # # weights.append(np.random.rand(20, 20))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (20, 20)))
+		# #weights.append(np.zeros(20,))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (20,)))
+		# # # weights.append(np.random.rand(20, 5))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (20, 5)))
+		# #weights.append(np.zeros(5,))
+		# weights.append(np.random.uniform(low = -1, high = 1.0, size = (5,)))
+		self.model.set_weights(self.weights)
 
 	def think(self, vision):
 		#Use the creatures Keras model and vision to determine direction/speed
