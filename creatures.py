ee38f20df54ce6e16f54d3475833ca60ba972fff
Max Potter <maxpotter0@gmail.com>
2017-06-24 13:59:30 +0100

    Added thinking to creatures
    Each creature now controls its own movement via a Mind object which uses a neural network.

--- creatures.py
+++ creatures.py
@@ -9,6 +9,7 @@ Description:
 import pygame
 from pygame.locals import *
 from random import randint, choice
+import minds as m
 
 #Lists of objects
 tigerList = pygame.sprite.Group()
@@ -30,7 +31,7 @@ class Creature(pygame.sprite.Sprite):
     Handles sprite initialisation and movement.
     """
 
-    def __init__(self, position, ctype):
+    def __init__(self, position, ctype, DNA = ''):
         pygame.sprite.Sprite.__init__(self)
         self.ctype = ctype
         self.name = self.get_name()
@@ -61,16 +62,50 @@ class Creature(pygame.sprite.Sprite):
         self.dx = 0
         self.dy = 0
 
-        #Vision
+        #Set up default vision (5x5 grid, all seeing 'off map')
         self.vision = [[-1 for column in range(5)] for row in range(5)]
 
+        #Create blank DNA and attach a Mind object to our creature
+        self.DNA = DNA
+        child = True if len(self.DNA) > 0 else False #children will have non-blank DNA strings
+        self.mind = m.Mind(firstGeneration = child, DNA = DNA)
+
     def update(self):
+        #Deplete energy and check if still alive!
         self.energy -= self.drainRate
         if self.energy <= 0:
             if self.ctype == 'tiger':
+                #*********************************************PASS ON DNA!!!!
                 tigerList.remove(self)
             if self.ctype == 'deer':
+                #*********************************************PASS ON DNA!!!!
                 deerList.remove(self)
+
+        #Feed vision into neural network and retrieve button presses
+        actions = self.mind.think(self.vision)
+
+        for action in actions: 
+            #Speed up: action[0] = K_SPACE
+            if int(round(action[0])) == 1::
+                self.speed = self.topSpeed
+            else:
+                self.speed = self.baseSpeed
+
+            #Move: 
+            if int(round(action[1])) == 1: #action[1] = K_w
+                self.dy -= 1
+            if int(round(action[2])) == 1: #action[2] = K_a
+                self.dx -= 1
+            if int(round(action[3])) == 1: #action[2] = K_s
+                self.dy += 1
+            if int(round(action[4])) == 1: #action[2] = K_d
+                self.dx += 1
+
+            #Which 'keys' are no longer pressed?
+            if int(round(action[1])) == 0 or int(round(action[3])) == 0:
+                self.dy = 0
+            if int(round(action[2])) == 0 or int(round(action[4])) == 0:
+                self.dx = 0
 
         self.rect.x += self.dx * self.speed
         self.rect.y += self.dy * self.speed
